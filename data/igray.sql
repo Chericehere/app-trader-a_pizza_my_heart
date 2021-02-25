3bc0416b7158a021c321e8692b819e7c5536b969
TheRealIanGray <therealiangray@gmail.com>
2021-02-24 20:18:14 -0600

    Working on sorting

--- data/igray.sql
+++ data/igray.sql
@@ -10,9 +10,21 @@ FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
 ON UPPER(a.name) = UPPER(p.name)*/
 
-
-select distinct(lower(a.name)), a.price, a.rating,a.content_rating,a.primary_genre, p.category, p.rating, p.size, p.install_count, p.type, p.price, p.content_rating, p.genres
+--Highest Rated with Install Base > 1 Million
+/*select distinct(lower(p.name)), cast(a.price as float), a.rating, p.rating, 
+			a.content_rating, cast(replace(replace(install_count, '+', ''), ',', '') as float) as in_co, 
+			p.type, p.price, p.content_rating, p.genres
 from app_store_apps as a
 inner join play_store_apps as p
 on lower(a.name) = lower(p.name)
-order by lower(a.name) desc
+where cast(a.price as float) <= 1.00 and cast(replace(replace(install_count, '+', ''), ',', '') as float) >= 1000000000
+order by p.rating desc, in_co desc*/
+
+select distinct(lower(p.name)), cast(a.price as float), a.rating, p.rating, 
+			a.content_rating, cast(replace(replace(install_count, '+', ''), ',', '') as float) as in_co, 
+			p.type, p.price, p.content_rating, p.genres
+from app_store_apps as a
+inner join play_store_apps as p
+on lower(a.name) = lower(p.name)
+where cast(a.price as float) <= 1.00 and cast(replace(replace(install_count, '+', ''), ',', '') as float) >= 500000000
+order by p.rating desc, in_co desc
