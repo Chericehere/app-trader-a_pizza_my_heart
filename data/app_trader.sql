9fe924d6b9001ff014e6e6888dc15cb4663738b9
mjlintemu <matthew.lintemuth@gmail.com>
2021-02-24 23:05:18 -0600

    baseline

--- data/app_trader.sql
+++ data/app_trader.sql
@@ -1,22 +1,31 @@
-SELECT a.name, CAST(a.price AS money), a.rating, a.primary_genre
-FROM app_store_apps AS a
-UNION
-SELECT p.name, CAST(p.price AS money), p.rating, p.genres
-FROM play_store_apps AS p
-WHERE rating IS NOT NULL
-ORDER BY rating DESC, price DESC
+SELECT * FROM app_store_apps
+limit 10
 
+SELECT * FROM play_store_apps
+limit 10
 
-SELECT name, genres
-from play_store_apps
+SELECT COUNT(name)
+FROM app_store_apps
+WHERE name IN (SELECT name 
+	  FROM play_store_apps )
+	  
+select * from play_Store_apps 
+limit 100
 
-SELECT name, primary_genre
+--Count overlap between the 2 datasets
+select sum((case when name in (select name from play_store_apps) then 1 else 0 end)) as overlap_ct, count(*) as apple_ct
 from app_store_apps
 
-SELECT name, CAST(price AS money), rating, a.primary_genre
-FROM app_store_apps
 
-SELECT p.name, CAST(p.price AS money), p.rating, p.genres
-FROM play_store_apps
-WHERE rating IS NOT NULL
-ORDER BY rating DESC, App_Price DESC
+--App name and price comparison between stores
+select a.name, a.price as apple_price, c.price  as google_price
+FROM app_store_apps a,
+	play_store_apps c
+where a.name = c.name 
+
+--incomplete
+SELECT name AS app_name, price AS app_price, review_count, rating, content_rating, primary_genre AS genre
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+ORDER BY price DESC
