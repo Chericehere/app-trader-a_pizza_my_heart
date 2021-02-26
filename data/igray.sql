ba2cb70575ba3f96ce03001b711f18e8d59e12c5
TheRealIanGray <therealiangray@gmail.com>
2021-02-25 21:21:05 -0600

    spliced by genre

--- data/igray.sql
+++ data/igray.sql
@@ -20,11 +20,56 @@ on lower(a.name) = lower(p.name)
 where cast(a.price as float) <= 1.00 and cast(replace(replace(install_count, '+', ''), ',', '') as float) >= 1000000000
 order by p.rating desc, in_co desc*/
 
-select distinct(lower(p.name)), cast(a.price as float), a.rating, p.rating, 
-			a.content_rating, cast(replace(replace(install_count, '+', ''), ',', '') as float) as in_co, 
-			p.type, p.price, p.content_rating, p.genres
+/*SELECT name, rating1,rating2,
+	ROUND((-10000 * price1) + (5000 * (rating1/.125)) + (-1000*(rating1/.125)),2) AS Net_income_by_rating,
+	ROUND((-10000 * price1) + (5000 * (rating2/.125)) + (-1000*(rating2/.125)),2) AS Net_income_by_rating2
+FROM
+	(SELECT DISTINCT(UPPER(a.name)) AS name, CAST(a.price AS NUMERIC(10,2))AS price1, a.rating AS rating1, a.content_rating AS c_rating1, a.primary_genre AS genre1, 
+									p.price AS price2, p.rating AS rating2, p.content_rating AS c_rating2, p.genres AS genre2
+	FROM app_store_apps AS a
+	INNER JOIN play_store_apps AS p
+	ON UPPER(a.name) = UPPER(p.name)) AS combo
+ORDER BY net_income_by_rating DESC
+*/
+
+/* with install base*/
+/*select distinct(lower(p.name)), cast(a.price as float), a.rating, p.rating, 
+			 cast(replace(replace(install_count, '+', ''), ',', '') as float) as in_co, 
+			p.type, p.price, cast(REPLACE(a.content_rating,'+','') as float)as age, p.genres
 from app_store_apps as a
 inner join play_store_apps as p
 on lower(a.name) = lower(p.name)
-where cast(a.price as float) <= 1.00 and cast(replace(replace(install_count, '+', ''), ',', '') as float) >= 500000000
-order by p.rating desc, in_co desc
+where cast(a.price as float) <= 1.00 /*and cast(replace(replace(install_count, '+', ''), ',', '') as float) >= 500000000*/
+order by a.rating desc/*, in_co desc*/*/
+
+/*count of titles by age rating*/
+/*select count(distinct(lower(p.name))), cast(REPLACE(a.content_rating,'+','') as float)as age/*, cast(a.price as float), a.rating, p.rating, 
+			 cast(replace(replace(install_count, '+', ''), ',', '') as float) as in_co, 
+			p.type, p.price, p.genres*/
+from app_store_apps as a
+inner join play_store_apps as p
+on lower(a.name) = lower(p.name)
+where cast(a.price as float) <= 1.00 and a.rating >= 4.5
+group by age*/
+
+select count(distinct(lower(p.name))) as title_count, cast(REPLACE(a.content_rating,'+','') as float)as age_group, p.genres/*, cast(a.price as float), a.rating, p.rating, 
+			 cast(replace(replace(install_count, '+', ''), ',', '') as float) as in_co, 
+			p.type, p.price, p.genres*/
+from app_store_apps as a
+inner join play_store_apps as p
+on lower(a.name) = lower(p.name)
+where cast(a.price as float) <= 1.00 and a.rating >= 4.5
+group by genres, age_group
+order by title_count desc, age_group desc
+
+select count(distinct(lower(p.name))) as title_count, /*cast(REPLACE(a.content_rating,'+','') as float)as age_group,*/ p.genres/*, cast(a.price as float), a.rating, p.rating, 
+			 cast(replace(replace(install_count, '+', ''), ',', '') as float) as in_co, 
+			p.type, p.price, p.genres*/
+from app_store_apps as a
+inner join play_store_apps as p
+on lower(a.name) = lower(p.name)
+where cast(a.price as float) <= 1.00 and a.rating >= 4.5
+group by genres
+order by title_count desc
+
+x
