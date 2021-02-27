6d48b13a57241da3aa142858182795b545015dba
mjlintemu <matthew.lintemuth@gmail.com>
2021-02-27 13:49:52 -0600

    dash

--- data/app_trader.sql
+++ data/app_trader.sql
@@ -11,6 +11,11 @@ FROM
 ORDER BY net_income_by_rating DESC
 
 
+SELECT name, SUM(10000 * CASE WHEN price <= 1 THEN 10000 END)
+FROM app_store_apps
+group by NAME
+
+
 --Count overlap between the 2 datasets
 select sum((case when name in (select name from play_store_apps) then 1 else 0 end)) as overlap_ct, count(*) as apple_ct
 from app_store_apps
@@ -28,13 +33,50 @@ FROM app_store_apps a,
 	play_store_apps p
 where a.name = p.name 
 
---Number of Genres
-SELECT count(DISTINCT primary_genre) FROM app_store_apps
 
-SELECT count(DISTINCT genres) FROM play_store_apps
+--Brenda's Query
+SELECT 
+		app, apple_price, google_price, apple_rating, google_rating, apple_content_rating, google_content_rating, apple_genre, google_genre, 
+		ROUND((-10000 * CASE WHEN apple_price <=1 THEN 1
+			  				 WHEN apple_price >1 THEN apple_price END )+ (5000 * (apple_rating/.125)) + (-1000*(apple_rating/.125)),2) AS net_income_by_apple_rating,
+		ROUND((-10000 * CASE WHEN google_price <=1 THEN 1
+			  				 WHEN google_price >1 THEN google_price END )+ (5000 * (google_rating/.125)) + (-1000*(google_rating/.125)),2) AS net_income_by_google_rating
+FROM
+	(SELECT 
+	 		DISTINCT(UPPER(a.name)) AS app, 
+	 		CAST(a.price AS NUMERIC(10,2))AS apple_price, CAST(REPLACE(p.price,'$','') AS NUMERIC(10,2)) AS google_price,
+	 		a.rating AS apple_rating, p.rating AS google_rating, 
+	 		a.content_rating  AS apple_content_rating, p.content_rating AS google_content_rating,
+			a.primary_genre AS apple_genre, p.genres AS google_genre	
+	 		
+	FROM app_store_apps AS a
+	INNER JOIN play_store_apps AS p
+	ON UPPER(a.name) = UPPER(p.name)) AS combo
+	ORDER BY net_income_by_apple_rating DESC, net_income_by_google_rating  DESC 
 
 
-SELECT a.name, a.rating, CAST(a.price AS numeric (10,2)), a.primary_genre, p.name, p.rating, CAST(p.price AS numeric(10,2)), p.genres
-FROM app_store_apps AS a,
-	play_store_apps AS p
-WHERE a.name = p.name
+
+--Brend'a Query Recoded
+SELECT
+		names_of_apps, apple_price, google_price, apple_rating, google_rating, apple_content_rating, google_content_rating, apple_genre, google_genre, google_install_count,
+		ROUND((-10000 * CASE WHEN apple_price <=1 THEN 1
+			  				 WHEN apple_price >1 THEN apple_price END )+ (5000 * (apple_rating/.125)) + (-1000*(apple_rating/.125)),2) AS net_income_by_apple_rating,
+		ROUND((-10000 * CASE WHEN google_price <=1 THEN 1
+			  				 WHEN google_price >1 THEN google_price END )+ (5000 * (google_rating/.125)) + (-1000*(google_rating/.125)),2) AS net_income_by_google_rating,
+		ROUND((-10000 * CASE WHEN apple_price <=1 THEN 1
+			  				 WHEN apple_price >1 THEN apple_price END )+ (5000 * (apple_rating/.125)) + (-1000*(apple_rating/.125)),2)
+	  		+ ROUND((-10000 * CASE WHEN google_price <=1 THEN 1
+								   WHEN google_price >1 THEN google_price END )+ (5000 * (google_rating/.125)) + (-1000*(google_rating/.125)),2)  AS total_net_income	
+FROM
+	(SELECT
+	 		DISTINCT(UPPER(a.name)) AS names_of_apps,
+	 		CAST(a.price AS NUMERIC(10,2))AS apple_price, CAST(REPLACE(p.price,'$','') AS NUMERIC(10,2)) AS google_price,
+	 		a.rating AS apple_rating, ceiling(cast(p.rating as NUMERIC)/.5)* .5 as google_rating,
+	 		a.content_rating  AS apple_content_rating, p.content_rating AS google_content_rating,
+	 		a.primary_genre AS apple_genre, p.genres AS google_genre,
+	 										CAST (REPLACE(REPLACE(p.install_count,'+',''),',','')AS INT) AS google_install_count
+	FROM app_store_apps AS a
+	INNER JOIN play_store_apps AS p
+	ON UPPER(a.name) = UPPER(p.name)) AS combo
+ORDER BY total_net_income DESC, names_of_apps
+LIMIT 10
